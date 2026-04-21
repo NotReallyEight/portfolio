d437b964eec8320825ffa657f296cb718e50480a
Antonio Wang <root@antoniowang.dev>
2026-04-21 22:07:33 +0200

    feat: add entry client for hydration and update react-router dependencies

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,10 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
+  future: {
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+  },
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
